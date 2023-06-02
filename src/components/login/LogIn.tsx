3a40a2baf1acafb416786d9a2c3d3f00ddd04cba
YaMamaLul <terentiy51@gmail.com>
2023-06-03 00:07:24 +0500

    RegistrationPC

--- src/components/login/LogIn.tsx
+++ src/components/login/LogIn.tsx
@@ -1,6 +1,10 @@
 import SiteHeader from "../../ui/header/SiteHeader";
 import styles from "./LogInHeader.module.scss"
 import SiteFooter from "../../ui/footer/SiteFooter";
+import yandexLogo from "../../imgs/yandexLogo.svg"
+import gitLogo from "../../imgs/githubLogo.svg"
+import googleLogo from "../../imgs/googleLogo.svg"
+
 const LogIn = () =>{
     return(
         <div className={styles.logWrapper}>
@@ -8,10 +12,15 @@ const LogIn = () =>{
             <div className={styles.formWrapper}>
                 <form className={styles.loginForm}>
                     <h3>Вход в личный кабинетик</h3>
-                    <p>Адрес электронной почты</p>
+                    <p>Адрес электронной почты <span>*</span></p>
                     <input className={styles.mailInput}/>
-                    <p>Пароль</p>
+                    <p>Пароль <span>*</span></p>
                     <input/>
+                    <div className={styles.socialMedias}>
+                        <img title="will be added later" src={yandexLogo} width="6%"/>
+                        <img title="will be added later" src={googleLogo} width="6%"/>
+                        <img title="will be added later" src={gitLogo} width="6%"/>
+                    </div>
                     <button>Войти</button>
                 </form>
             </div>
--- src/components/registration/Registration.tsx
+++ src/components/registration/Registration.tsx
@@ -1,26 +1,32 @@
 import SiteHeader from "../../ui/header/SiteHeader";
-import styles from "../login/LogInHeader.module.scss";
+import styles from "../registration/Registration.module.scss";
 import SiteFooter from "../../ui/footer/SiteFooter";
+import yandexLogo from "../../imgs/yandexLogo.svg";
+import googleLogo from "../../imgs/googleLogo.svg";
+import gitLogo from "../../imgs/githubLogo.svg";
 
 const Registration = () =>{
     return(
-        <div>
+        <div className={styles.regWrapper}>
             <SiteHeader isLogged={false} userName="nothing" />
-            <div className={styles.loginPage}>
-                <h3>Добрый вечер, регаемся, пожлауйста</h3>
-                <div className={styles.regForm}>
-                    <p>Ваше имя</p>
-                    <input></input>
-                    <p>Адрес электронной почты</p>
-                    <input></input>
-                    <p>Пароль</p>
-                    <input></input>
-                    <p>Подтверждение пароля</p>
-                    <input></input>
-                    <input type="checkbox"></input>
-                    <p>Принимаю все, что написано в том документе большом</p>
-                    <button>Регистер</button>
-                </div>
+            <div className={styles.formWrapper}>
+                <form className={styles.regForm}>
+                    <h3>Регистрация</h3>
+                    <p>Имя</p>
+                    <input/>
+                    <p>Адрес электронной почты <span>*</span></p>
+                    <input className={styles.mailInput}/>
+                    <p>Пароль <span>*</span></p>
+                    <input/>
+                    <p>Повторите пароль <span>*</span></p>
+                    <input/>
+                    <div className={styles.socialMedias}>
+                        <img title="will be added later" src={yandexLogo} width="6%"/>
+                        <img title="will be added later" src={googleLogo} width="6%"/>
+                        <img title="will be added later" src={gitLogo} width="6%"/>
+                    </div>
+                    <button>Войти</button>
+                </form>
             </div>
             <SiteFooter></SiteFooter>
         </div>
