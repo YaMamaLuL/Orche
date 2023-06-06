ce933e998bcdbba2db0b131e7c8a2f8ffb0cee7d
YaMamaLul <terentiy51@gmail.com>
2023-06-06 18:48:06 +0500

    PC

--- src/components/personalOffice/OfficeBody.tsx
+++ src/components/personalOffice/OfficeBody.tsx
@@ -1,8 +1,19 @@
-import EmptyOffice from "./EmptyOffice/EmptyOffice";
+import styles from "./OfficeBody.module.scss"
+import eoStyles from "./EmptyOffice/EmptyOffice.module.scss"
+import {useState} from "react";
+
+const EmptyOffice = () =>{
+    return(
+        <div className={eoStyles.emptyOfficeWrapper}>
+            <p>Укажите ссылку на ваше приложение</p>
+            <input/>
+            <button>Запустить оркестратор</button>
+        </div>)
+}
 
 
 const OfficeBody = () =>{
-    return(<div>
+    return(<div className={styles.OfficeWrapper}>
         <EmptyOffice/>
     </div>)
 }
