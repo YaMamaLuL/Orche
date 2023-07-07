3251900a99dfe77203c2b8e4c1b018b1b9c19a17
YaMamaLul <terentiy51@gmail.com>
2023-07-07 22:42:30 +0500

    PC

--- src/components/personalOffice/OfficeBody.tsx
+++ src/components/personalOffice/OfficeBody.tsx
@@ -1,38 +1,59 @@
 import styles from "./OfficeBody.module.scss"
 import eoStyles from "./EmptyOffice.module.scss"
 import officestyles from "./Office.module.scss"
-
 import axios from 'axios'
-import React, {useState} from "react";
-import Redirect from 'react-router-dom'
+import React, {useEffect, useState} from "react";
 import Cookies from "universal-cookie";
 import {Simulate} from "react-dom/test-utils";
 import error = Simulate.error;
 
-const getServiceNames = async (projectId:string, accessToken: string) => {
-    await axios.get(`http://192.168.0.104:3000/api/projects/${projectId}/services`,
-        {headers:{"accept": "application/json", "Authorizathion":`Bearer ${accessToken}`}}).
-    then((responce)=>{return responce.data})
-        .catch((error) => {console.log(error); return [{id:0,name:"nothing",status:error}]})
-}
-
 const OfficeBody = () =>{
-    const cookie = new Cookies()
+    const cookies = new Cookies()
     const [hasProjects, setHasProjects] = useState(false)
-    const [serviceName, setServiceName] = useState("")
+    const [currentService, setCurrentService] = useState("")
     const [serviceNames, setServiceNames] = useState([{id:"",name:"", repository:""}])
     const [projectNames, setProjectNames] = useState([{id:"",name:""}])
+    const [currentProject, setCurrentProject] = useState({id:"", name:""})
 
-    useState(()=>{
-        axios.get("http://192.168.0.104:3000/api/projects", {headers:{
-            "accept":"application/json", "Authorization": `Bearer ${cookie.get("accessToken")}`
+    const projectClickHandle = async (project: {id:string, name:string}) => {
+        setCurrentProject(project)
+
+        axios.get(`http://192.168.0.104:3000/api/projects/${project.id}/services`,
+            {headers:{"accept":"json/application", "Authorization":`Bearer ${cookies.get("accessToken")}`}})
+            .then((responce) => {
+                console.log(responce.data)
+                setServiceNames(responce.data)
+            })
+    }
+
+    const serviceClickHandle = async (service:{id:string,name:string, repository:string}) => {
+        axios.get(`http://192.168.0.104:3000/api/projects/${currentProject.id}/services/${service.id}`,
+            {headers:{
+                "accept":"aзplication/json", "Authorization": `Bearer ${cookies.get("accessToken")}`
+                }})
+    }
+
+    const getProjectNames = async() => {
+        await axios.get("http://192.168.0.104:3000/api/projects", {headers:{
+                "accept":"application/json", "Authorization": `Bearer ${cookies.get("accessToken")}`
             }})
             .then((responce) =>{
-                setHasProjects(responce.data.length > 0)
-                console.log(responce.data)
                 setProjectNames(responce.data)
             })
-    })
+            .catch((error) => {return [{id:"", name:""}]})
+    }
+
+    const getServiceNames = async (projectId:string) => {
+        await axios.get(`http://192.168.0.104:3000/api/projects/${projectId}/services`,
+            {headers:{"accept": "application/json", "Authorization":`Bearer ${cookies.get("accessToken")}`}}).
+        then((responce)=>{return responce.data})
+            .catch((error) => {console.log(error); return [{id:0,name:"nothing",status:error}]})
+    }
+
+    useEffect(()=>{
+      getProjectNames()
+      setHasProjects(projectNames.length > 0)
+    },[])
 
     if (hasProjects){
         return(
@@ -40,18 +61,27 @@ const OfficeBody = () =>{
                 <div className={officestyles.projectNamesWrapper}>
                     <h1>Проект</h1>
                     <ul>
-                        {projectNames.map(item => <li>{item.name}</li>)}
+                        {projectNames[0].id !== ""
+                            ? projectNames.map((item, key) => <li key={key}><button onClick={async (event) => {
+                                console.log(item.id)
+                                await projectClickHandle(item)
+                                event.preventDefault()}}>{item.name}</button></li>)
+                            : <p>Создайте свой первый проект!</p>}
+                        <li key={projectNames.length+1}><button>Создать проект</button></li>
                     </ul>
                 </div>
                 <div className={officestyles.projectNamesWrapper}>
                     <h1>Приложение</h1>
                     <ul>
-                        {serviceNames.map(item => <li>{item.name}</li>)}
+                        {currentProject.name !== ""
+                            ? serviceNames.map((item,key) => <li key={key}><button>{item.name}</button></li>)
+                            : <li><p>Список приложений пуст</p></li>}
+                        <li key={serviceNames.length+1}><button>Создать приложение</button></li>
                     </ul>
                 </div>
                 <div className={officestyles.officeWrapper}>
                     <h1>Управление приложением</h1>
-                    <p>Имя проекта: {serviceName}</p>
+                    <p>Имя приложения: {currentService}</p>
                     <p>Состояние: {"1231"}</p>
                     <span>Ссылка на ваше приложение:</span>
                     <a href={"1231"}>{"1231"}</a>
